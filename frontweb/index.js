b2f6c981075f1600e52917db0f7c3ffd11e20883
primael <primael@l-infini.fr>
2015-07-15 19:09:57 +0200

    FrontWeb
    Ajout de l'élément de recherche

--- frontweb/index.js
+++ frontweb/index.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(logger({path: "gestcopro_front.log"}));
 app.use(bodyParser.urlencoded({ extended: false }));
--- frontweb/public/js/views/search.js
+++ frontweb/public/js/views/search.js
@@ -9,11 +9,44 @@ window.SearchView = Backbone.View.extend({
         return this;
     },
 
+    events: {
+        "click .search"   : "beforeSearch",
+        "click .allSearch" : "changeItemAll",
+        "click .userSearch" : "changeItemUser",
+        "click .buildingSearch" : "changeItemBuilding",
+    },
+
     selectMenuItem: function (menuItem) {
         $('.nav li').removeClass('active');
         if (menuItem) {
             $('.' + menuItem).addClass('active');
         }
-    }
+    },
 
+    beforeSearch: function () {
+        //On lance la recherche non?
+        this.search();
+        return false;
+    },
+
+    search: function () {        
+
+        console.log('élément recherché : ' + $('#textSearch').val() + ' zone de recherche : ' + $('#zoneSearch').attr("data-zone"));
+        
+    },
+
+    changeItemAll: function(){
+        console.log('élément recherché : ' + $('#textSearch').val() + ' zone de recherche : ' + $('#zoneSearch').attr("data-zone"));
+        $('#zoneSearch').removeClass().addClass('glyphicon glyphicon-menu-hamburger').attr('data-zone','ALL');
+    },
+
+    changeItemUser: function(){
+        console.log('élément recherché : ' + $('#textSearch').val() + ' zone de recherche : ' + $('#zoneSearch').attr("data-zone"));
+        $('#zoneSearch').removeClass().addClass('glyphicon glyphicon-user').attr('data-zone','USER');
+    },
+
+    changeItemBuilding: function(){
+        console.log('élément recherché : ' + $('#textSearch').val() + ' zone de recherche : ' + $('#zoneSearch').attr("data-zone"));
+        $('#zoneSearch').removeClass().addClass('glyphicon glyphicon-stats').attr('data-zone','BUILDING');
+    },
 });
